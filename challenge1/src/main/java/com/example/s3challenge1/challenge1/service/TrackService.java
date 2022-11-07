bd6b8129d22ce5e9861774bea424f363d949a689
Monali Sharma <mona.chaudhari444@gmail.com>
2022-11-07 19:17:01 +0530

    mc complete

---
+++ challenge1/src/main/java/com/example/s3challenge1/challenge1/service/TrackService.java
@@ -0,0 +1,17 @@
+package com.example.s3challenge1.challenge1.service;
+
+import com.example.s3challenge1.challenge1.domain.Track;
+import com.example.s3challenge1.challenge1.exception.TrackNotFoundException;
+
+import java.util.List;
+
+public interface TrackService {
+
+    Track addTrack(Track track);
+    List<Track> getAllTrack() throws Exception;
+
+    boolean deleteTrackById(int trackId) throws TrackNotFoundException;
+    List<Track> getTrackGreaterThan4(int trackRating) throws TrackNotFoundException;
+
+    List<Track> getTrackByArtistName(String artistName) throws TrackNotFoundException;
+}
---
+++ challenge1/src/main/java/com/example/s3challenge1/challenge1/service/TrackServiceImpl.java
@@ -0,0 +1,50 @@
+package com.example.s3challenge1.challenge1.service;
+
+import com.example.s3challenge1.challenge1.domain.Track;
+import com.example.s3challenge1.challenge1.exception.TrackNotFoundException;
+import com.example.s3challenge1.challenge1.repository.TrackRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrackServiceImpl implements TrackService{
+
+    private TrackRepository trackRepository;
+
+    public TrackServiceImpl(TrackRepository trackRepository){
+        this.trackRepository = trackRepository;
+    }
+    @Override
+    public Track addTrack(Track track) {
+        return trackRepository.save(track);
+    }
+
+    @Override
+    public List<Track> getAllTrack() {
+        return trackRepository.findAll();
+    }
+
+    @Override
+    public boolean deleteTrackById(int trackId) throws TrackNotFoundException {
+        boolean result = false;
+
+        if(trackRepository.findById(trackId).isEmpty()){
+            throw new TrackNotFoundException();
+        }else {
+            trackRepository.deleteById(trackId);
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public List<Track> getTrackGreaterThan4(int trackRating) {
+        return trackRepository.findByTrackRating(trackRating);
+    }
+
+    @Override
+    public List<Track> getTrackByArtistName(String artistName) {
+        return trackRepository.findAllTrackByArtistName(artistName);
+    }
+}
